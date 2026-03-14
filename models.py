b60c7ec4b7c2ef9dfce8be3114efbeaabe4b19f8
0xkadirx0 <142223183+0xkadirx0@users.noreply.github.com>
2026-03-14 03:59:10 +0300

    Create models.py

---
+++ models.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class Room(Base):
+    __tablename__ = "rooms"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    price = Column(Integer)

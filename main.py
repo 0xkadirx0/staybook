47da4d6442a2d505d3d87006ca7daa4a8c89129f
0xkadirx0 <142223183+0xkadirx0@users.noreply.github.com>
2026-03-14 04:04:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import services
 
-app = FastAPI()
+app = FastAPI(
+    title="StayBook API",
+    description="Hotel Room Recommendation System",
+    version="1.0"
+)
 
 app.add_middleware(
     CORSMiddleware,
@@ -12,17 +16,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-@app.get("/")
+@app.get("/", tags=["Root"])
 def read_root():
     return {"message": "StayBook API running"}
 
-
-@app.get("/rooms")
+@app.get("/rooms", tags=["Rooms"])
 def get_rooms():
     return services.get_rooms()
 
-
-@app.get("/recommend")
+@app.get("/recommend", tags=["Recommendation"])
 def recommend_room(budget: int):
     return services.recommend_room(budget)
